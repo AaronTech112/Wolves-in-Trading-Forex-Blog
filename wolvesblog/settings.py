e834306e173f3f1f4391ba52c57c11b93d5b0e45
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-25 22:48:03 +0100

    Update settings.py

--- wolvesblog/settings.py
+++ wolvesblog/settings.py
@@ -130,14 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/upload/'
+STATIC_URL = os.path.join(BASE_DIR,'/static/')
+MEDIA_URL = os.path.join(BASE_DIR,'/upload/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/upload')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
