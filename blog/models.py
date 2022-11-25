f16fabcd0c16a19ce2e85d5aeb0b789dffea900f
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-25 16:18:50 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null = True)
-    username = models.CharField(max_length=200, null = True)
+    username = models.CharField(unique=True,max_length=200, null = True)
     email = models.EmailField(unique=True, null=True)
     profile = models.ImageField(null=True,default='avatar.svg')
     
-    USERNAME_FIELD = 'email'    
-    REQUIRED_FIELDS = []
+#     USERNAME_FIELD = 'email'    
+#     REQUIRED_FIELDS = []
 
 class Category(models.Model):
     name = models.CharField(max_length = 200)
