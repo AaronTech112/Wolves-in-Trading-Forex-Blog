d71e1b454707689c18300d2ccce6e844ca892030
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-25 22:40:50 +0100

    Update settings.py

--- wolvesblog/settings.py
+++ wolvesblog/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/upload/'
 
-MEDIA_ROOT = BASE_DIR / 'static/upload'
+MEDIA_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
