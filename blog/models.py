8550305fe54b3d9b9ea90995eaf35738c3b5fa4a
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-26 15:42:13 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=200, null = True)
     email = models.EmailField(unique=True, null=True)
-    profile = models.ImageField(null=True, upload_to='/upload/' ,default='avatar.svg')
+    profile = models.ImageField(null=True, upload_to=' ' ,default='avatar.svg')
     
     USERNAME_FIELD = 'email'    
     REQUIRED_FIELDS = []
@@ -23,7 +23,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=1000)
     subtitle= models.CharField(null=True,max_length=1000)
-    image = models.ImageField(null=True,upload_to='/upload/' ,default='bg1.png')
+    image = models.ImageField(null=True,upload_to=' ' ,default='bg1.png')
     body = models.TextField(null=True)
     Links = models.TextField(null=True)
     LinkTitle = models.CharField(null=True,max_length=1000)
