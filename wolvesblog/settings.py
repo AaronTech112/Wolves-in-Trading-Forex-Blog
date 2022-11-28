6d4dcde930bfe2f199078f81933edf776befcf2d
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-28 14:16:03 +0100

    Update settings.py

--- wolvesblog/settings.py
+++ wolvesblog/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT =os.path.join(BASE_DIR,'static/upload/')
-MEDIA_URL = '/upload/'
+MEDIA_ROOT =os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
